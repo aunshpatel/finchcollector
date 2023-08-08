0131fa52ad355fed399bbdbd3d6a5a3896dbfa0e
aunshpatel <patelaunsh28@gmail.com>
2023-08-07 20:28:23 -0400

    Dynamic data display

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finchs_index(request):
+    finchs = Finch.objects.all()
+    return render(request, 'finchs/index.html',{
+        'finchs':finchs
+    })
