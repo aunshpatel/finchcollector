80acc819e941aeb29d3152cc68d9eea6238d6bc9
aunshpatel <patelaunsh28@gmail.com>
2023-08-08 14:04:24 -0400

    Add, edit and delete finch functionailty

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('finch/', views.finchs_index, name='index'),
+    path('finchs/', views.finchs_index, name='index'),
+    path('finchs/<int:finch_id>/', views.finchs_detail, name='detail'),
+    path('finchs/create/', views.FinchCreate.as_view(), name='finchs_create'),
+    path('finchs/<int:pk>/update', views.FinchUpdate.as_view(), name='finchs_update'),
+    path('finchs/<int:pk>/delete', views.FinchDelete.as_view(), name='finchs_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 def home(request):
@@ -13,3 +14,21 @@ def finchs_index(request):
     return render(request, 'finchs/index.html',{
         'finchs':finchs
     })
+
+def finchs_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finchs/detail.html',{
+        'finch':finch
+    })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields=['name','color','description']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url='/finchs'
