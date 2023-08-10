a82e63fbdd22fd12f3b069bf09e89eb5f1aa02a5
aunshpatel <patelaunsh28@gmail.com>
2023-08-09 22:52:36 -0400

    Add, update, delete toys with list & add to finch

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -8,16 +9,30 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+  
+
 class Finch(models.Model):
     name =  models.CharField(max_length=100)
     color = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    
-    def __str__(self) -> str:
+    toys = models.ManyToManyField(Toy)
+    def __str__(self):
         return f'{self.name} ({self.id})'
-    
+
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'finch_id':self.id})
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
     
 class Feeding(models.Model):
